4f208f5a5d0e147b931fe28448bff2b3f8286856
Franquex <162130969+Franquex@users.noreply.github.com>
2025-04-04 11:00:57 -0400

    Franco Orellana Calderon 04/04
    Franco Orellana Calderon 04/04

---
+++ main.java
@@ -0,0 +1,14 @@
+import pojos.Ejercicio6;
+
+public class main {
+    public static void main(String[] args) {
+        //Ejercicio5 ejercicio5 = new Ejercicio5();
+        //ejercicio5.ejecutar();
+
+        //Ejercicio6 ejercicio6 = new Ejercicio6();
+        //ejercicio6.ejecutar();
+
+        //Ejercicio7 ejercicio7 = new Ejercicio7();
+        //ejercicio7.ejecutar();
+    }
+}
